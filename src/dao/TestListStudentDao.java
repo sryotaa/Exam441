1e852e41f9be8f3215b94f8e671191c7282760cd
AikenRAITO5390 <knz2374426@stu.o-hara.ac.jp>
2024-04-25 14:59:38 +0900

    林変更

---
+++ src/dao/TestListStudentDao.java
@@ -0,0 +1,13 @@
+package dao;
+
+public class TestListStudentDao extends Dao {
+
+	/**
+	 * baseSql:String 共通SQL文 プライベート
+	 */
+	private String baseSql = "select * from test where school_cd=? ";
+
+
+
+
+}
