7b8f56d1f936bbf2d011b4524ecd88e12d600037
1205ki <knz2374470@stu.o-hara.ac.jp>
2024-05-07 10:37:46 +0900

    ログアウト機能・コメントの追加

--- src/scoremanager/main/LogoutAction.java
+++ src/scoremanager/main/LogoutAction.java
@@ -18,9 +18,18 @@ public class LogoutAction extends Action{
 		//なし
 		//ビジネスロジック 4
 
-		//Sessionを無効にする
+		//Sessionが存在するか確認	→	存在する＝現在のセッションを取得(◇１)
+		//								存在しない＝nullを返す(◆２)
 		HttpSession session = req.getSession(false);
-		session.invalidate();
+
+		if (session != null) {
+			//Sessionが存在する場合(◇１)
+			System.out.println("セッションの存在を確認しました。ログアウトします。");
+			session.invalidate();	//Sessionを削除(テキストｐ269)
+		} else {
+			//Sessionが存在しなかった場合(◆２)
+			System.out.println("セッションは存在しない為、ログインできません。");
+		}
 
 		//DBへデータ保存 5
 		//なし
