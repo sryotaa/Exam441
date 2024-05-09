9ba1eedb3523f862ccf83d7e6a0f534ac255d744
ocha <ochamaru8@icloud.com>
2024-05-09 10:42:06 +0900

    科目変更したああ

--- WebContent/scoremanager/main/subject_update_done.jsp
+++ WebContent/scoremanager/main/subject_update_done.jsp
@@ -1,5 +1,7 @@
+<%-- 学生更新完了JSP --%>
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
+<%@taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
@@ -7,8 +9,8 @@
 <title>得点管理システム</title>
 </head>
 <body>
-
-	<h2>科目情報削除</h2>
-
+	<h2>科目情報変更</h2>
+		<p>変更が完了しました</p>
+	<a href="SubjectList.action">科目一覧</a>
 </body>
 </html>
--- src/scoremanager/main/SubjectCreateAction.java
+++ src/scoremanager/main/SubjectCreateAction.java
@@ -1,5 +1,42 @@
 package scoremanager.main;
 
-public class SubjectCreateAction {
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import bean.Teacher;
+import dao.ClassNumDao;
+import tool.Action;
+
+public class SubjectCreateAction extends Action{
+
+	@Override
+	public void execute(HttpServletRequest req, HttpServletResponse res) throws Exception {
+		//ローカル変数の宣言 1
+		HttpSession session = req.getSession(true);// セッションを取得
+		ClassNumDao cNumDao = new ClassNumDao();// クラス番号Daoを初期化
+		Teacher teacher = (Teacher) session.getAttribute("user");// ログインユーザーを取得
+		LocalDate todaysDate = LocalDate.now();// LcalDateインスタンスを取得
+		int year = todaysDate.getYear();// 現在の年を取得
+		List<Integer> entYearSet = new ArrayList<>();//入学年度のリストを初期化
+
+		//リクエストパラメータ―の取得 2
+		//なし
+
+		//DBからデータ取得 3
+		List<String> list = cNumDao.filter(teacher.getSchool());// ログインユーザーの学校コードをもとにクラス番号の一覧を取得
+
+
+		//レスポンス値をセット 6
+		req.setAttribute("class_num_set", list);//クラス番号のlistをセット
+		req.setAttribute("ent_year_set", entYearSet);//入学年度のlistをセット
+
+		//JSPへフォワード 7
+		req.getRequestDispatcher("subject_create.jsp").forward(req, res);
+	}
 
 }
